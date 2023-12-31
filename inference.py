80a2252c5f5ecced0ff0549032dbf44e7d6c8566
Nuked <nuked8@gmail.com>
2023-12-31 17:24:50 +0100

    implemented ignore on fail

--- inference.py
+++ inference.py
@@ -87,16 +87,19 @@ def face_detect(images):
 	results = []
 	pady1, pady2, padx1, padx2 = args.pads
 	for rect, image in zip(predictions, images):
-		if rect is None:
-			cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
-			raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+		try:
+			if rect is None:
+				cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
+				raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
 
-		y1 = max(0, rect[1] - pady1)
-		y2 = min(image.shape[0], rect[3] + pady2)
-		x1 = max(0, rect[0] - padx1)
-		x2 = min(image.shape[1], rect[2] + padx2)
-		
-		results.append([x1, y1, x2, y2])
+			y1 = max(0, rect[1] - pady1)
+			y2 = min(image.shape[0], rect[3] + pady2)
+			x1 = max(0, rect[0] - padx1)
+			x2 = min(image.shape[1], rect[2] + padx2)
+			
+			results.append([x1, y1, x2, y2])
+		except:
+			pass
 
 	boxes = np.array(results)
 	if not args.nosmooth: boxes = get_smoothened_boxes(boxes, T=5)
@@ -119,29 +122,31 @@ def datagen(frames, mels):
 		face_det_results = [[f[y1: y2, x1:x2], (y1, y2, x1, x2)] for f in frames]
 
 	for i, m in enumerate(mels):
-		idx = 0 if args.static else i%len(frames)
-		frame_to_save = frames[idx].copy()
-		face, coords = face_det_results[idx].copy()
+		try:
+			idx = 0 if args.static else i%len(frames)
+			frame_to_save = frames[idx].copy()
+			face, coords = face_det_results[idx].copy()
 
-		face = cv2.resize(face, (args.img_size, args.img_size))
-			
-		img_batch.append(face)
-		mel_batch.append(m)
-		frame_batch.append(frame_to_save)
-		coords_batch.append(coords)
+			face = cv2.resize(face, (args.img_size, args.img_size))
+				
+			img_batch.append(face)
+			mel_batch.append(m)
+			frame_batch.append(frame_to_save)
+			coords_batch.append(coords)
 
-		if len(img_batch) >= args.wav2lip_batch_size:
-			img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
+			if len(img_batch) >= args.wav2lip_batch_size:
+				img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
 
-			img_masked = img_batch.copy()
-			img_masked[:, args.img_size//2:] = 0
+				img_masked = img_batch.copy()
+				img_masked[:, args.img_size//2:] = 0
 
-			img_batch = np.concatenate((img_masked, img_batch), axis=3) / 255.
-			mel_batch = np.reshape(mel_batch, [len(mel_batch), mel_batch.shape[1], mel_batch.shape[2], 1])
+				img_batch = np.concatenate((img_masked, img_batch), axis=3) / 255.
+				mel_batch = np.reshape(mel_batch, [len(mel_batch), mel_batch.shape[1], mel_batch.shape[2], 1])
 
-			yield img_batch, mel_batch, frame_batch, coords_batch
-			img_batch, mel_batch, frame_batch, coords_batch = [], [], [], []
-
+				yield img_batch, mel_batch, frame_batch, coords_batch
+				img_batch, mel_batch, frame_batch, coords_batch = [], [], [], []
+		except:
+			print("box error")
 	if len(img_batch) > 0:
 		img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
 
