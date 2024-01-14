3dc24ba374e76c18b2dba5ef264a598aa5d4a93b
Nuked <nuked8@gmail.com>
2024-01-14 18:14:47 +0100

    added auto models downloading

--- infer.bat
+++ infer.bat
@@ -1,7 +1,5 @@
 @echo off
 
-set /p VIDEO_PATH=Enter the path to your video file (e.g., C:\path\to\your\video\file.mp4):
-set /p AUDIO_PATH=Enter the path to your audio file (e.g., C:\path\to\your\audio\file.wav):
 
 IF NOT EXIST venv (
     python -m venv venv
@@ -16,6 +14,11 @@ echo venv %PYTHON%
 %PYTHON% -m pip install -r cuda_requirements.txt
 %PYTHON% -m pip install -r requirements.txt
 
+%PYTHON% "%~dp0\download_models.py"
+
+
+set /p VIDEO_PATH=Enter the path to your video file (e.g., C:\path\to\your\video\file.mp4):
+set /p AUDIO_PATH=Enter the path to your audio file (e.g., C:\path\to\your\audio\file.wav):
 
 echo Video Path: %VIDEO_PATH%
 echo Audio Path: %AUDIO_PATH%
